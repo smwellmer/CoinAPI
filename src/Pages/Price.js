c357e4d229d200a6722ce1013be29b8f3f6c9900
Samantha Wellmer <sam@Samanthas-MacBook-Air.local>
2021-07-27 12:48:56 -0400

    all the things

---
+++ src/Pages/Price.js
@@ -0,0 +1,44 @@
+import React, {useState, useEffect} from 'react'
+
+const Price = (props) => {
+
+    const apiKey = "E5DEEB86-11F9-483D-86AF-57DF1216B814"
+
+    const symbol = props.match.params.symbol
+
+    const url =  `http://rest.coinapi.io/v1/exchangerate/${symbol}/USD?apikey=${apiKey}`;
+
+    const [coin, setCoin] = useState(null)
+
+    const getCoin = async () => {
+        const response = await fetch(url);
+        const data = await response.json();
+        setCoin(data)
+    }
+
+    useEffect(() => {
+        getCoin()
+    }, [])
+
+    const loaded = () => {
+        return (
+        <div>
+            <h1>
+                {coin.asset_id_base}/{coin.asset_id_quote}
+            </h1>
+            <h2>
+                {coin.rate}
+            </h2>
+        </div>
+        )
+    }
+
+    const loading = () => {
+        return <h1>Loading...</h1>
+    }
+
+    return coin ? loaded() : loading()
+    
+}
+
+export default Price
